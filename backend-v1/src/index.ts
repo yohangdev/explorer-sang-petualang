742eaad0edd1459a3619c7eb90ed52ccc4830cf2
Yoga Hanggara <yohang88@gmail.com>
2025-08-21 17:37:46 +0700

    Refactor Elysia app initialization for cleaner chaining and update imports formatting

--- backend-v1/src/index.ts
+++ backend-v1/src/index.ts
@@ -1,25 +1,22 @@
-import { Elysia } from "elysia";
-import { swagger } from '@elysiajs/swagger'
+import {Elysia} from "elysia";
+import {swagger} from '@elysiajs/swagger'
 
-const app = new Elysia();
-
-app.use(swagger({
-    documentation: {
-        info: {
-            title: 'Explorer Sang Petualang API',
-            description: 'Explorer Sang Petualang API description',
-            version: '1.0.0'
-        },
-    }
-}));
-
-app.get("/up", () => "Application is running.", {
-    detail: {
-        summary: 'Get application health status.',
-        tags: ['App']
-    }
-});
-
-app.listen(8080);
+const app = new Elysia()
+    .use(swagger({
+        documentation: {
+            info: {
+                title: 'Explorer Sang Petualang API',
+                description: 'Explorer Sang Petualang API description',
+                version: '1.0.0'
+            },
+        }
+    }))
+    .get("/up", () => "Application is running.", {
+        detail: {
+            summary: 'Get application health status.',
+            tags: ['App']
+        }
+    })
+    .listen(8080);
 
 console.log(`🦊 Backend Service v1 is running at ${app.server?.hostname}:${app.server?.port}`);
