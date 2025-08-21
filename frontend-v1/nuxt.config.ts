a0f45b2d4660f082a6849ff61964343b6b3979b4
Yoga Hanggara <yohang88@gmail.com>
2025-08-21 22:06:37 +0700

    Set up initial Nuxt.js structure for `frontend-v1` with TailwindCSS, added default layout, homepage, and updated dependencies.

--- frontend-v1/nuxt.config.ts
+++ frontend-v1/nuxt.config.ts
@@ -2,5 +2,6 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxtjs/tailwindcss']
+  modules: ['@nuxt/eslint', '@nuxtjs/tailwindcss'],
+    // pages: true,
 })
